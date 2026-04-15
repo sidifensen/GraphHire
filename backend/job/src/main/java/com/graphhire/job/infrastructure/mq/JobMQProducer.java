53437ab7ed9f5443e50931aa4fe6e76ffa22a9cd
sidifensen <1848221808@qq.com>
2026-04-15 22:14:18 +0800

    feat: complete job module DDD structure

---
+++ backend/job/src/main/java/com/graphhire/job/infrastructure/mq/JobMQProducer.java
@@ -0,0 +1,21 @@
+package com.graphhire.job.infrastructure.mq;
+
+import com.graphhire.job.domain.event.JobPublishedEvent;
+import com.graphhire.job.domain.model.Job;
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JobMQProducer {
+
+    @Autowired
+    private RocketMQTemplate rocketMQTemplate;
+
+    private static final String TOPIC_JOB_PUBLISHED = "job-published";
+
+    public void sendJobPublishedEvent(Job job) {
+        JobPublishedEvent event = new JobPublishedEvent(job);
+        rocketMQTemplate.convertAndSend(TOPIC_JOB_PUBLISHED, event);
+    }
+}
