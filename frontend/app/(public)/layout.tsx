3ad62cd6e8668c9130955776fe96333dc04431e6
sidifensen <1848221808@qq.com>
2026-04-16 19:17:28 +0800

    feat: add public route group layout

---
+++ frontend/app/(public)/layout.tsx
@@ -0,0 +1,11 @@
+import { Header, Footer } from '@/components/shared/layout';
+
+export default function PublicLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="min-h-screen flex flex-col bg-gray-50 dark:bg-gray-950">
+      <Header />
+      <main className="flex-1">{children}</main>
+      <Footer />
+    </div>
+  );
+}
