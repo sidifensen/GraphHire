d812032d2e2d0ceb730d83bdc573c67ec631a01b
sidifensen <1848221808@qq.com>
2026-04-17 01:26:19 +0800

    feat(frontend): 添加共享布局组件 PublicHeader, PublicFooter, SideNav, TopBar, GlassCard, StatCard

--- frontend/components/shared/layout/index.ts
+++ frontend/components/shared/layout/index.ts
@@ -1,4 +1,6 @@
-export { Header } from './Header';
-export { PublicHeader } from './PublicHeader';
-export { Footer } from './Footer';
-export { Container } from './Container';
+export { default as PublicHeader } from './PublicHeader';
+export { default as PublicFooter } from './PublicFooter';
+export { default as SideNav } from './SideNav';
+export { default as TopBar } from './TopBar';
+export { default as GlassCard } from './GlassCard';
+export { default as StatCard } from './StatCard';
